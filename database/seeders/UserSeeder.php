985dcf036829f4fcd5d0d7a13d1243c55db286a5
Richard <williot.richard@gmail.com>
2021-01-27 17:35:23 +0100

    seed user 2

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,12 +15,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $user = Hash::make('utilisateur');
+        $mod = Hash::make('moderateur');
+        $admin = Hash::make('administrateur');
+        $sadmin = Hash::make('sadministrateur');
+        
+        DB::table('Users')->delete();
         $users = [
-            ['username' => 'utilisateur', 'password' => 'utilisateur', 'first_name' => 'utilisateur', 'last_name' => 'utilisateur', 'email' => 'utilisateur@cesi.fr', 'grade_id' => 1],
-            ['username' => 'moderateur', 'password' => 'moderateur', 'first_name' => 'moderateur', 'last_name' => 'moderateur', 'email' => 'moderateur@cesi.fr', 'grade_id' => 2],
-            ['username' => 'administrateur', 'password' => 'administrateur', 'first_name' => 'administrateur', 'last_name' => 'administrateur', 'email' => 'administrateur@cesi.fr', 'grade_id' => 3],
-            ['username' => 'sadministrateur', 'password' => 'sadministrateur', 'first_name' => 'sadministrateur', 'last_name' => 'sadministrateur', 'email' => 'sadministrateur@cesi.fr', 'grade_id' => 4],
+            ['username' => 'utilisateur', 'password' => $user, 'first_name' => 'utilisateur', 'last_name' => 'utilisateur', 'email' => 'utilisateur@cesi.fr', 'grade_id' => 1],
+            ['username' => 'moderateur', 'password' => $mod, 'first_name' => 'moderateur', 'last_name' => 'moderateur', 'email' => 'moderateur@cesi.fr', 'grade_id' => 2],
+            ['username' => 'administrateur', 'password' => $admin, 'first_name' => 'administrateur', 'last_name' => 'administrateur', 'email' => 'administrateur@cesi.fr', 'grade_id' => 3],
+            ['username' => 'sadministrateur', 'password' => $sadmin, 'first_name' => 'sadministrateur', 'last_name' => 'sadministrateur', 'email' => 'sadministrateur@cesi.fr', 'grade_id' => 4],
         ];
         DB::table('Users')->insert($users);
     }
 }
+Hash::make();
