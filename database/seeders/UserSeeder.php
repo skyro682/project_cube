aa6ca570b53989f3ee6995b7a15e46e8613a0831
Alexis Milano <alexis.milano68@gmail.com>
2021-01-27 17:41:06 +0100

    fix user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,20 +14,42 @@ class UserSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        $user = Hash::make('utilisateur');
-        $mod = Hash::make('moderateur');
-        $admin = Hash::make('administrateur');
-        $sadmin = Hash::make('sadministrateur');
-        
+    {        
         DB::table('Users')->delete();
         $users = [
-            ['username' => 'utilisateur', 'password' => $user, 'first_name' => 'utilisateur', 'last_name' => 'utilisateur', 'email' => 'utilisateur@cesi.fr', 'grade_id' => 1],
-            ['username' => 'moderateur', 'password' => $mod, 'first_name' => 'moderateur', 'last_name' => 'moderateur', 'email' => 'moderateur@cesi.fr', 'grade_id' => 2],
-            ['username' => 'administrateur', 'password' => $admin, 'first_name' => 'administrateur', 'last_name' => 'administrateur', 'email' => 'administrateur@cesi.fr', 'grade_id' => 3],
-            ['username' => 'sadministrateur', 'password' => $sadmin, 'first_name' => 'sadministrateur', 'last_name' => 'sadministrateur', 'email' => 'sadministrateur@cesi.fr', 'grade_id' => 4],
+            [
+                'username' => 'utilisateur',
+                'password' => Hash::make('utilisateur'),
+                'first_name' => 'utilisateur',
+                'last_name' => 'utilisateur',
+                'email' => 'utilisateur@cesi.fr',
+                'grade_id' => 1
+            ],
+            [
+                'username' => 'moderateur', 
+                'password' => Hash::make('moderateur'), 
+                'first_name' => 'moderateur', 
+                'last_name' => 'moderateur', 
+                'email' => 'moderateur@cesi.fr', 
+                'grade_id' => 2
+            ],
+            [
+                'username' => 'administrateur', 
+                'password' => Hash::make('administrateur'), 
+                'first_name' => 'administrateur', 
+                'last_name' => 'administrateur', 
+                'email' => 'administrateur@cesi.fr', 
+                'grade_id' => 3
+            ],
+            [
+                'username' => 'sadministrateur', 
+                'password' => Hash::make('sadministrateur'), 
+                'first_name' => 'sadministrateur', 
+                'last_name' => 'sadministrateur', 
+                'email' => 'sadministrateur@cesi.fr', 
+                'grade_id' => 4
+            ],
         ];
         DB::table('Users')->insert($users);
     }
 }
-Hash::make();
