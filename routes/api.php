6c521ffb96d70d5d3c30db3cead11de85c07a708
Alexis Milano <alexis.milano68@gmail.com>
2021-01-28 17:19:45 +0100

    add auth trought api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [\App\Http\Controllers\API\AuthController::class, 'register']);
+Route::post('login', [\App\Http\Controllers\API\AuthController::class, 'login']);
+Route::post('logout', [\App\Http\Controllers\API\AuthController::class, 'logout']);
+
+Route::get('ressources', [\App\Http\Controllers\API\RessourcesController::class, 'index']);
+Route::get('ressources/{id}', [\App\Http\Controllers\API\RessourcesController::class, 'show']);
+
+Route::middleware('auth:api')->group(function(){
+
+    Route::get('/user', function (Request $request) { return $request->user(); });
+    Route::apiResource('ressources', \App\Http\Controllers\API\RessourcesController::class);
+
 });
