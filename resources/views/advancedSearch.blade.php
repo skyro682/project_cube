c7dac8e82f699a0b6d2dd6417e47db472860912c
ZetaXo-dev <etiennecrenner@hotmail.fr>
2021-01-29 12:18:06 +0100

    Mise en page

--- resources/views/advancedSearch.blade.php
+++ resources/views/advancedSearch.blade.php
@@ -18,35 +18,45 @@
                                value="{{ $_GET['contentQuery'] }}"
                             @endif>
                         <br>
-                        <label for="categorySelect">Catégorie</label>
-                        <select class="form-control" id="categorySelect" name="category">
-                            <option value="all">Toutes</option>
-                            @foreach($categoriesList as $category)
-                                @if(isset($category->id))
-                                    <option value={{ $category->id }}>{{ $category->name }}</option>
-                                @endif
-                            @endforeach
-                        </select>
-                        <br>
-                        <label for="regionSelect">Région</label>
-                        <select class="form-control" id="regionSelect" name="region">
-                            <option value="all">Toutes</option>
-                            @foreach($regionsList as $region)
-                                @if(isset($region->id))
-                                    <option value={{ $region->id }}>{{ $region->name }}</option>
-                                @endif
-                            @endforeach
-                        </select>
-                        <br>
-                        <label for="orderSelect">Affichage des posts</label>
-                        <select class="form-control" id="orderSelect" name="order">
-                            <option value=0>Derniers créés d'abord</option>
-                            <option value=1>Premiers créés d'abord</option>
-                            <option value=2>Premiers modifiés d'abord</option>
-                            <option value=3>Derniers modifiés d'abord</option>
-                            <option value=2>Les plus vus d'abord</option>
-                            <option value=3>Les moins vus d'abord</option>
-                        </select>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Catégorie</label>
+                            </div>
+                            <select name="zone_id" class="custom-select" id="inputGroupSelect01">
+                                <option value="{{ (isset($ressource)) ? $ressource->$category->id : '' }}">{{ (isset($ressource)) ? $ressource->$category->name : 'Choisir une option...' }}</option>
+                                @foreach ($categoriesList as $category)
+
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Région</label>
+                            </div>
+                            <select name="zone_id" class="custom-select" id="inputGroupSelect01">
+                                <option value="{{ (isset($ressource)) ? $ressource->region->id : '' }}">{{ (isset($ressource)) ? $ressource->region->name : 'Choisir une option...' }}</option>
+                                @foreach ($regionsList as $region)
+
+                                    <option value="{{ $region->id }}">{{ $region->name }}</option>
+
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Ordre</label>
+                            </div>
+                            <select name="zone_id" class="custom-select" id="inputGroupSelect01">
+                                <option value=0>{{ "Derniers créés d'abord" }}</option>
+                                <option value=1>{{ "Premiers créés d'abord" }}</option>
+                                <option value=2>{{ "Premiers modifiés d'abord" }}</option>
+                                <option value=3>{{ "Derniers modifiés d'abord" }}</option>
+                                <option value=4>{{ "Les plus vus d'abord" }}</option>
+                                <option value=5>{{ "Les moins vus d'abord" }}</option>
+                            </select>
+                        </div>
                         <br>
                         <button type="submit" class="btn btn-info">Rechercher</button>
                         <a class="btn btn-info" href="{{ route('search') }}" role="button">Recherche Simple</a>
