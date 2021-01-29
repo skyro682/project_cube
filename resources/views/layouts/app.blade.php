142d13e6e164f3ea5793899e97e0e737e05062fb
Richard <williot.richard@gmail.com>
2021-01-29 16:45:14 +0100

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" id="mainNav">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    (Re)sources Relationnelles
+                    (Re)ssources Relationnelles
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -124,7 +124,6 @@
         <main class="py-4">
             @yield('content')
         </main>
-        <br>
         <hr>
         <footer class="bg-light text-center text-lg-start w-100 " style="bottom: 0;">
             <div class="container p-3" style="margin-bottom: 40px;">
